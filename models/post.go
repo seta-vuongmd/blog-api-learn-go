57cec53b9b9c3198dae4402b81d59146940d3959
seta-vuongmd <vuong.mai@seta-international.vn>
2025-09-18 15:48:14 +0700

    Add .gitignore and update Post model to use pq.StringArray for tags

--- models/post.go
+++ models/post.go
@@ -1,11 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/lib/pq"
+	"gorm.io/gorm"
+)
 
 type Post struct {
 	gorm.Model
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
-	Tags    []string `json:"tags" gorm:"type:text[]"`
-	UserID  uint     `json:"user_id"`
+	Title   string         `json:"title"`
+	Content string         `json:"content"`
+	Tags    pq.StringArray `json:"tags" gorm:"type:text[]"`
+	UserID  uint           `json:"user_id"`
 }
